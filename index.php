0c34f3d0bb065cdd1d408238a4ce2c9b78a4037b
Ade Zulkarnain <ade.zulkarnain13@gmail.com>
2022-09-01 10:46:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sistem Akademik SMAN 1 Bangun Raya</title>
+<body>
+    <header>
+        <h1><center>Sistem Akademik SMAN 1 Bangun Raya</center></h1>
+    </header>
+<?php
+    $bilangan = 5;
+    print "Bilangan adalah ".$bilangan;
+    print "</br>";
+    if ( $bilangan%2 == 0 ) {
+    print "Bilangan Genap";
+    } else {
+    print "Bilangan Ganjil";
+    }
+?>
+</body>
+    </head>
+</html>
